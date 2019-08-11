90d0d93c29c31b6c9326bb29c0b9bf2b8e3819d5
Emre Koc <hi@emroni.com>
2019-08-11 17:26:04 +0700

    Animate 1

--- src/hexagon/1.js
+++ src/hexagon/1.js
@@ -1,9 +1,10 @@
 import * as PIXI from 'pixi.js';
 import Recorder from '../inc/recorder';
 
+const PI2 = Math.PI * 2;
 const SIZE = 30;
-const SPACING = 7;
-const ROWS = 13;
+const SPACING = 5;
+const ROWS = 14;
 
 const app = new PIXI.Application({
     antiAlias: true,
@@ -26,28 +27,50 @@ shape.lineTo(-0.4330125 * SIZE, 0.25 * SIZE);
 shape.endFill();
 const texture = app.renderer.generateTexture(shape);
 
-function add(x, y) {
-    const hex = new PIXI.Sprite.from(texture);
-    container.addChild(hex);
-    hex.position.x = (x - 2.625) * ((SIZE * 0.4330125) + SPACING) + app.view.width / 2;
-    hex.position.y = (y - 0.5) * ((SIZE * 0.75) + SPACING) + app.view.height / 2;
-}
+const rows = [];
 
 let x = 0, y = 0;
 for (let i = 0; i <= ROWS; i++) {
-    add(x += 2, y);
-    for (let j = 0; j < i - 1; j++) add(++x, ++y);
-    for (let j = 0; j < i; j++) add(--x, ++y);
-    for (let j = 0; j < i; j++) add(x -= 2, y);
-    for (let j = 0; j < i; j++) add(--x, --y);
-    for (let j = 0; j < i; j++) add(++x, --y);
-    for (let j = 0; j < i; j++) add(x += 2, y);
+    const row = [];
+    rows.push(row);
+
+    add(row, x += 2, y);
+    for (let j = 0; j < i - 1; j++) add(row, ++x, ++y);
+    for (let j = 0; j < i; j++) add(row, --x, ++y);
+    for (let j = 0; j < i; j++) add(row, x -= 2, y);
+    for (let j = 0; j < i; j++) add(row, --x, --y);
+    for (let j = 0; j < i; j++) add(row, ++x, --y);
+    for (let j = 0; j < i; j++) add(row, x += 2, y);
+}
+
+function add(row, x, y) {
+    x = (x - 2.625) * ((SIZE * 0.4330125) + SPACING) + app.view.width / 2;
+    y = (y - 0.55) * ((SIZE * 0.75) + SPACING) + app.view.height / 2;
+
+    if (x <= -SIZE || x >= app.view.width || y < -SIZE || y >= app.view.height) {
+        return;
+    }
+
+    const hex = new PIXI.Sprite.from(texture);
+    container.addChild(hex);
+    row.push(hex);
+
+    hex.position.x = x;
+    hex.position.y = y;
 }
 
 new Recorder({
     duration: 5,
     target: app.view,
     render: (tick) => {
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i];
+            const alpha = Math.abs(Math.sin(PI2 * (tick - i / rows.length)));
 
+            for (let j = 0; j < row.length; j++) {
+                let hex = row[j];
+                hex.alpha = alpha;
+            }
+        }
     },
 });
