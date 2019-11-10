29675b038029c7f898199e46400196485955b5c7
Emre Koc <hi@emroni.com>
2019-11-10 16:30:26 +0700

    Add size/scale option to mp4.sh

--- bin/mp4.sh
+++ bin/mp4.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ffmpeg -framerate 60 -i public/export/temp/%04d.png -pix_fmt yuv420p -c:v libx264 -crf 1 -framerate 60 public/export/$1.mp4
+if [ -n "$2" ];
+    then SCALE="-filter:v scale=-1:$2";
+    else SCALE='';
+fi
+
+ffmpeg -framerate 60 -i public/export/temp/%04d.png -pix_fmt yuv420p -c:v libx264 -crf 1 -framerate 60 $SCALE public/export/$1.mp4
