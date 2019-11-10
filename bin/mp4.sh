2d199c94bd3cef0611835f29f71292e2a8ff132b
Emre Koc <hi@emroni.com>
2019-11-10 17:20:03 +0700

    Make mp4.sh overwrite without prompt

--- bin/mp4.sh
+++ bin/mp4.sh
@@ -5,4 +5,4 @@ if [ -n "$2" ];
     else SCALE='';
 fi
 
-ffmpeg -framerate 60 -i public/export/temp/%04d.png -pix_fmt yuv420p -c:v libx264 -crf 1 -framerate 60 $SCALE public/export/$1.mp4
+ffmpeg -framerate 60 -i public/export/temp/%04d.png -pix_fmt yuv420p -c:v libx264 -crf 1 -framerate 60 -y $SCALE public/export/$1.mp4
