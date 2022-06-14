0cba710395fafaca627ae8bb3e6143c3c1aff02d
Emre Koc <hi@emrekoc.io>
2022-06-14 15:58:33 +0700

    Remove history api usage

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -14,9 +14,3 @@ interface Experiment {
     size: number;
     onTick(time: number): void;
 }
-
-interface SidebarItem {
-    anchor: HTMLAnchorElement;
-    element: HTMLLIElement;
-    url: string;
-}
--- src/components/Sidebar.ts
+++ src/components/Sidebar.ts
@@ -1,7 +1,5 @@
 import experiments from '@/experiments';
 
-let items: SidebarItem[];
-
 window.addEventListener('load', () => {
     // Get elements
     const list = document.getElementById('sidebar-list');
@@ -13,7 +11,7 @@ window.addEventListener('load', () => {
 
     // Generate items
     const names = Object.keys(experiments);
-    items = names.map((name, index) => {
+    names.forEach((name, index) => {
         const url = `/${name}`;
 
         // Clone element
@@ -23,34 +21,14 @@ window.addEventListener('load', () => {
         // Update anchor
         const anchor = element.querySelector('a');
         anchor.setAttribute('href', url);
-        anchor.addEventListener('click', e => {
-            e.preventDefault();
-            window.history.pushState({}, '', url);
-            const popStateEvent = new PopStateEvent('popstate', { state: {} });
-            window.dispatchEvent(popStateEvent);
-        });
 
         const content = anchor.querySelectorAll('span');
         content[0].innerHTML = (names.length - index).toString().padStart(3, '0');
         content[1].innerHTML = name.replace('/', ' / ');
 
-        // Return item
-        return {
-            anchor,
-            element,
-            url,
-        };
+        const active = window.location.pathname === url;
+        anchor.classList.toggle('hover:bg-slate-600', !active);
+        anchor.classList.toggle('bg-slate-700', active);
+        anchor.classList.toggle('pointer-events-none', active);
     });
-
-    window.addEventListener('popstate', updateItems);
-    updateItems();
 });
-
-function updateItems() {
-    items.forEach(item => {
-        const active = window.location.pathname === item.url;
-        item.anchor.classList.toggle('hover:bg-slate-600', !active);
-        item.anchor.classList.toggle('bg-slate-700', active);
-        item.anchor.classList.toggle('pointer-events-none', active);
-    });
-}
