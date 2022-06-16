a277a60e562d119c97b8a1701edd3857012a14a6
Emre Koc <hi@emrekoc.io>
2022-06-16 17:45:02 +0700

    Add Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import '../styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
 
-    return <Component {...pageProps} />;
+    return <div className="flex h-screen">
+        <main className="bg-zinc-900 flex flex-col h-screen flex-1 overflow-hidden text-white">
+            <Component {...pageProps} />
+        </main>
+    </div>;
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        './src/**/*.{css,html,ts,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['Open Sans', 'sans-serif'],
+        },
+    },
+};
