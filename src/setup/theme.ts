818c3a67a25b1ebbddea4ca68e92c90d2902bab8
Emre Koc <hi@emrekoc.io>
2024-07-20 16:32:26 +0200

    Add Work Sans font

--- src/setup/theme.ts
+++ src/setup/theme.ts
@@ -1,7 +1,11 @@
+import '@fontsource-variable/work-sans';
 import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
     palette: {
         mode: 'dark',
+    },
+    typography: {
+        fontFamily: 'Work Sans Variable, sans-serif',
     },
 });
