5609b1a24265b8493f0c44759482c66a7f0c11ca
Emre Koc <hi@emrekoc.io>
2024-07-29 13:00:06 +0200

    Add controls reset

--- src/components/ExperimentControls/ExperimentControls.tsx
+++ src/components/ExperimentControls/ExperimentControls.tsx
@@ -1,12 +1,26 @@
 'use client';
-import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
-import { Box, Button, Slider, Typography } from '@mui/material';
+import { Close, Settings, SettingsBackupRestore } from '@mui/icons-material';
+import { Box, IconButton, Slider, Typography } from '@mui/material';
 import _ from 'lodash';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ExperimentControls({ items, onChange }: ExperimentControlsProps) {
 
+    const [initial, setInitial] = useState<Map<string, number>>();
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (!initial) {
+            const newInitial = new Map();
+            Object.entries(items).forEach(([name, item]) => {
+                newInitial.set(name, item.value);
+            });
+            setInitial(newInitial);
+        }
+    }, [
+        initial,
+        items,
+    ]);
 
     function handleChange(name: string, value: number) {
         const newItems = { ...items };
@@ -14,45 +28,54 @@ export default function ExperimentControls({ items, onChange }: ExperimentContro
         onChange(newItems);
     }
 
+    function handleReset() {
+        const newItems = { ...items };
+        initial?.forEach((value, name) => {
+            newItems[name].value = value;
+        });
+        onChange(newItems);
+    }
+
     return <Box color="grey.500" position="absolute" right={0} top={0} zIndex={1}>
+        {!open && (
+            <IconButton color="inherit" title="Controls" onClick={() => setOpen(true)}>
+                <Settings />
+            </IconButton>
+        )}
         {open && (
-            <Box bgcolor="grey.900" padding={2} width={200}>
-                {Object.entries(items).map(([name, item]) => (
-                    <Box key={name}>
-                        <Box display="flex" justifyContent="space-between">
-                            <Typography>
-                                {_.startCase(name)}
-                            </Typography>
-                            <Typography>
-                                {item.value.toLocaleString()}
-                            </Typography>
+            <Box bgcolor="grey.900">
+                <Box textAlign="right">
+                    <IconButton color="inherit" title="Reset" onClick={handleReset}>
+                        <SettingsBackupRestore />
+                    </IconButton>
+                    <IconButton color="inherit" title="Close" onClick={() => setOpen(false)}>
+                        <Close />
+                    </IconButton>
+                </Box>
+                <Box paddingX={2} width={200}>
+                    {Object.entries(items).map(([name, item]) => (
+                        <Box key={name}>
+                            <Box display="flex" justifyContent="space-between">
+                                <Typography>
+                                    {_.startCase(name)}
+                                </Typography>
+                                <Typography>
+                                    {item.value.toLocaleString()}
+                                </Typography>
+                            </Box>
+                            <Slider
+                                max={item.max}
+                                min={item.min}
+                                size="small"
+                                step={item.step || ((item.max - item.min) / 100)}
+                                value={item.value}
+                                onChange={(_, value) => handleChange(name, value as number)}
+                            />
                         </Box>
-                        <Slider
-                            max={item.max}
-                            min={item.min}
-                            size="small"
-                            step={item.step || ((item.max - item.min) / 100)}
-                            value={item.value}
-                            onChange={(_, value) => handleChange(name, value as number)}
-                        />
-                    </Box>
-                ))}
+                    ))}
+                </Box>
             </Box>
         )}
-        <Box textAlign="right">
-            <Button
-                color="inherit"
-                endIcon={open ? (
-                    <KeyboardArrowUp />
-                ) : (
-                    <KeyboardArrowDown />
-                )}
-                size="small"
-                onClick={() => setOpen(!open)}
-            >
-                Controls
-            </Button>
-        </Box>
     </Box>;
 
 }
