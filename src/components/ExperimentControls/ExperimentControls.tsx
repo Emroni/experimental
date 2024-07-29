3b3ac5d0dd598b4789246c92d77050c2fbd40094
Emre Koc <hi@emrekoc.io>
2024-07-29 11:01:44 +0200

    Add controls toggle

--- src/components/ExperimentControls/ExperimentControls.tsx
+++ src/components/ExperimentControls/ExperimentControls.tsx
@@ -1,7 +1,12 @@
-import { Box, Slider, Typography } from '@mui/material';
+'use client';
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
+import { Box, Button, Slider, Typography } from '@mui/material';
 import _ from 'lodash';
+import { useState } from 'react';
 
 export default function ExperimentControls({ items, onChange }: ExperimentControlsProps) {
+
+    const [open, setOpen] = useState(false);
 
     function handleChange(name: string, value: number) {
         const newItems = { ...items };
@@ -9,27 +14,45 @@ export default function ExperimentControls({ items, onChange }: ExperimentContro
         onChange(newItems);
     }
 
-    return <Box bgcolor="grey.900" padding={2} position="absolute" right={0} top={0} width={200} zIndex={1}>
-        {Object.entries(items).map(([name, item]) => (
-            <Box key={name}>
-                <Box display="flex" justifyContent="space-between">
-                    <Typography>
-                        {_.startCase(name)}
-                    </Typography>
-                    <Typography>
-                        {item.value.toLocaleString()}
-                    </Typography>
-                </Box>
-                <Slider
-                    max={item.max}
-                    min={item.min}
-                    size="small"
-                    step={item.step || ((item.max - item.min) / 100)}
-                    value={item.value}
-                    onChange={(_, value) => handleChange(name, value as number)}
-                />
+    return <Box color="grey.500" position="absolute" right={0} top={0} zIndex={1}>
+        {open && (
+            <Box bgcolor="grey.900" padding={2} width={200}>
+                {Object.entries(items).map(([name, item]) => (
+                    <Box key={name}>
+                        <Box display="flex" justifyContent="space-between">
+                            <Typography>
+                                {_.startCase(name)}
+                            </Typography>
+                            <Typography>
+                                {item.value.toLocaleString()}
+                            </Typography>
+                        </Box>
+                        <Slider
+                            max={item.max}
+                            min={item.min}
+                            size="small"
+                            step={item.step || ((item.max - item.min) / 100)}
+                            value={item.value}
+                            onChange={(_, value) => handleChange(name, value as number)}
+                        />
+                    </Box>
+                ))}
             </Box>
-        ))}
+        )}
+        <Box textAlign="right">
+            <Button
+                color="inherit"
+                endIcon={open ? (
+                    <KeyboardArrowUp />
+                ) : (
+                    <KeyboardArrowDown />
+                )}
+                size="small"
+                onClick={() => setOpen(!open)}
+            >
+                Controls
+            </Button>
+        </Box>
     </Box>;
 
 }
