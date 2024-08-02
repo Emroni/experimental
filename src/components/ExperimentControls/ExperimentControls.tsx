f46d4830b11b1bd7595047fd9930caa5d1a7c090
Emre Koc <hi@emrekoc.io>
2024-08-02 10:30:16 +0200

    Improve ExperimentControls value formatting

--- src/components/ExperimentControls/ExperimentControls.tsx
+++ src/components/ExperimentControls/ExperimentControls.tsx
@@ -2,7 +2,7 @@
 import { Close, Settings, SettingsBackupRestore } from '@mui/icons-material';
 import { Box, IconButton, Slider, Typography } from '@mui/material';
 import _ from 'lodash';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function ExperimentControls({ items, onChange }: ExperimentControlsProps) {
 
@@ -36,6 +36,26 @@ export default function ExperimentControls({ items, onChange }: ExperimentContro
         onChange(newItems);
     }
 
+    const elements = useMemo(() => {
+        return Object.entries(items).map(([name, item]) => {
+            const step = item.step || ((item.max - item.min) / 100);
+            const decimals = parseInt(step.toString().split('.')[1]) || 0;
+
+            return {
+                ...item,
+                formattedValue: item.value.toLocaleString(undefined, {
+                    maximumFractionDigits: decimals,
+                    minimumFractionDigits: decimals,
+                }),
+                label: _.startCase(name),
+                name,
+                step,
+            }
+        });
+    }, [
+        items,
+    ]);
+
     return <Box color="grey.500" position="absolute" right={0} top={0} zIndex={1}>
         {!open && (
             <IconButton color="inherit" title="Controls" onClick={() => setOpen(true)}>
@@ -53,23 +73,23 @@ export default function ExperimentControls({ items, onChange }: ExperimentContro
                     </IconButton>
                 </Box>
                 <Box paddingX={2} width={200}>
-                    {Object.entries(items).map(([name, item]) => (
-                        <Box key={name}>
+                    {elements.map(element => (
+                        <Box key={element.name}>
                             <Box display="flex" justifyContent="space-between">
                                 <Typography>
-                                    {_.startCase(name)}
+                                    {element.label}
                                 </Typography>
                                 <Typography>
-                                    {item.value.toLocaleString()}
+                                    {element.formattedValue}
                                 </Typography>
                             </Box>
                             <Slider
-                                max={item.max}
-                                min={item.min}
+                                max={element.max}
+                                min={element.min}
                                 size="small"
-                                step={item.step || ((item.max - item.min) / 100)}
-                                value={item.value}
-                                onChange={(_, value) => handleChange(name, value as number)}
+                                step={element.step}
+                                value={element.value}
+                                onChange={(_, value) => handleChange(element.name, value as number)}
                             />
                         </Box>
                     ))}
